a51996d112e3fd4eacec4f404e545706eec5d551
oneillde216 <31212974+oneillde216@users.noreply.github.com>
2018-04-13 17:37:04 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Fat Guy Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Triple Cheeseburger');
---
+++ server.js
@@ -0,0 +1,35 @@
+
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var router = require("./controllers/burgers_controller.js");
+var path = require("path");
+
+
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+app.use(methodOverride("_method"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+app.use(express.static(__dirname + "/public"));
+app.use("/", router);
+
+
+
+
+app.listen(PORT, function() {
+	console.log("Listening on PORT " + PORT);
+});
